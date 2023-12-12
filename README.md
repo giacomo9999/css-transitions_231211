0a7914cf3c042145c5e7dfd9a0731a08bbaec558
giacomo9999 <gmarchesi888@gmail.com>
2023-12-12 09:48:21 -0500

    Added README.

---
+++ README.md
@@ -0,0 +1,1 @@
+Accordion-style image display using Flexbox.
--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 console.log("I am the script.");
- 
+
+const panels = document.querySelectorAll(".panel");
+
+const removeActiveClasses = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
